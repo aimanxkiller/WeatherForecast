f5071006cc27a318e4dd8d11a978191c6aa37bfe
aimanxkiller <aimanxkiller@gmail.com>
2023-04-10 15:28:53 +0800

    Initial commit

---
+++ app/src/main/java/com/example/weatherforecast/fragment/WeatherFragment.kt
@@ -0,0 +1,83 @@
+package com.example.weatherforecast.fragment
+
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.LocationListener
+import android.location.LocationManager
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.core.app.ActivityCompat
+import androidx.fragment.app.Fragment
+import com.example.weatherforecast.R
+
+class WeatherFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_weather, container, false)
+    }
+
+    private lateinit var locationManager: LocationManager
+    private lateinit var locationListener: LocationListener
+
+    private var latitude:Double? = null
+    private var longitude:Double? = null
+
+    private lateinit var textV:TextView
+
+    @SuppressLint("SetTextI18n")
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        textV = view.findViewById(R.id.testText)
+
+        locationManager = requireContext().getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        locationListener = LocationListener { location ->
+
+            //Get Lat and Long here
+            latitude = String.format("%.3f", location.latitude).toDouble()
+            longitude = String.format("%.3f",location.longitude).toDouble()
+
+            textV.text = "Lat:$latitude & Long:$longitude"
+
+        }
+
+        checkLocation()
+
+    }
+
+    private fun checkLocation(){
+        if (ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED &&
+            ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            locationManager.requestLocationUpdates(
+                LocationManager.GPS_PROVIDER,
+                0,
+                0f,
+                locationListener
+            )
+        } else {
+            ActivityCompat.requestPermissions(
+                requireActivity(),
+                arrayOf(
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ),
+                1
+            )
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/weatherforecast/ui/MainActivity.kt
@@ -0,0 +1,71 @@
+package com.example.weatherforecast.ui
+
+import android.Manifest.permission.ACCESS_FINE_LOCATION
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import com.example.weatherforecast.R
+
+//Do weather app similar to https://dribbble.com/shots/17288933-Kawaii-Weather-App-Exploration
+//API Link as follows "https://api.open-meteo.com/v1/forecast?latitude=3.14&longitude=101.69&hourly=temperature_2m,precipitation_probability&current_weather=true&forecast_days=7&timezone=auto"
+//Location by Lat+Long (3 decimal place) , forecast_day should be a variable
+
+//MVVM,HILT,ViewModel, Nav Comp with 1 Fragment only
+//Handle error when no lat is entered and show error to user
+
+class MainActivity : AppCompatActivity() {
+
+    private val PERMISSION_REQUEST_CODE = 101
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        checkLocationPermissions()
+
+    }
+
+    // Check for permission status and request permissions if not granted
+    private fun checkLocationPermissions() {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this, arrayOf(
+                    ACCESS_FINE_LOCATION,
+                    android.Manifest.permission.ACCESS_COARSE_LOCATION
+                ),
+                PERMISSION_REQUEST_CODE
+            )
+        }
+    }
+
+    // Override onRequestPermissionsResult to handle permission requests
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode == PERMISSION_REQUEST_CODE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permission granted, do something here
+
+            } else {
+                // Permission denied, show a message to the user or handle it in some other way
+            }
+        }
+    }
+
+}
