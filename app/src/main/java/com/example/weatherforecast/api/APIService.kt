650677e142324f54fb8031459d33ae63d720ac76
aimanxkiller <aimanxkiller@gmail.com>
2023-04-11 06:59:04 +0800

    TODO( - Get City name from lat/long - Get weather API - Get BotSheetFrag for future forecast - MotionLayout for top app )

--- app/src/main/java/com/example/weatherforecast/api/APIService.kt
+++ app/src/main/java/com/example/weatherforecast/api/APIService.kt
@@ -7,6 +7,8 @@ import retrofit2.http.GET
 interface APIService {
 
     companion object{
+        //Expected url blow
+        //https://api.open-meteo.com/v1/forecast?latitude=3.14&longitude=101.69&hourly=temperature_2m,precipitation_probability&current_weather=true&forecast_days=7&timezone=auto
         const val BASE_URL = "https://api.open-meteo.com/v1/forecast?"
     }
 
